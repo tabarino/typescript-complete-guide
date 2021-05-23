1b8728831b8ca8c7f71e6d1754362919c22a269c
Ivan Tabarino <tabarino@outlook.com>
2021-05-23 19:55:49 +0100

    Using Types

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here!!!');
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = '2.8';
+
+// + in this case converts the variables on numbers
+const result = add(+number1, +number2);
+
+console.log(result);
