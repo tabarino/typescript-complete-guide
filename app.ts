3ca17c0fdc3ab2ae039e5571836b5a1ad94937bc
Ivan Tabarino <tabarino@outlook.com>
2021-05-23 20:24:52 +0100

    Working with Numbers, Strings and Booleans

--- app.ts
+++ app.ts
@@ -1,11 +1,16 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, print: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (print) {
+    console.log(phrase, result);
+  }
+
+  return result;
 }
 
 const number1 = '5';
 const number2 = '2.8';
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
 // + in this case converts the variables on numbers
-const result = add(+number1, +number2);
-
-console.log(result);
+add(+number1, +number2, printResult, resultPhrase);
