5d3c71faff8f297f9fd5085fe3f99a2e3ca157e7
Ivan Tabarino <tabarino@outlook.com>
2021-05-26 12:01:29 +0100

    Default Function Parameters

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,6 @@ const clickHandler = (message: string) => {
 if (button) {
   button.addEventListener('click', clickHandler.bind(null, 'You are welcome!'));
 }
+
+const add = (n1: number, n2: number = 1) => n1 + n2;
+console.log(add(2));
