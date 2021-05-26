ddd3459d636ea50d199a73912c893dd36a0b7133
Ivan Tabarino <tabarino@outlook.com>
2021-05-26 12:35:43 +0100

    The Spread Operator

--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,18 @@ if (button) {
 
 const add = (n1: number, n2: number = 1) => n1 + n2;
 console.log(add(2));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking', ...hobbies];
+// activeHobbies.push(...hobbies);
+
+console.log(activeHobbies);
+
+const person = {
+  name: 'Ivan',
+  age: 39
+};
+
+const copiedPerson = { ...person };
+
+console.log(copiedPerson);
