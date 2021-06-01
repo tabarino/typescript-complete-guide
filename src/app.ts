5d5e07cab301b1af3b7e4d422836ecb714087f06
Ivan Tabarino <tabarino@outlook.com>
2021-06-01 19:07:22 +0100

    Nullish Coalescing

--- src/app.ts
+++ src/app.ts
@@ -12,3 +12,13 @@ const fetchedUserData = {
 
 // This is on Typescript (Optional Chaining)
 console.log(fetchedUserData?.job?.title);
+
+
+// Nullish Coalescing
+const userInput = null;
+
+// We would in this way in Javascript
+// const storedData = userInput || 'Default';
+
+// This is on Typescript (Nullish Coalescing) - It only checks if it is null or undefined
+const storedData = userInput ?? 'Default';
