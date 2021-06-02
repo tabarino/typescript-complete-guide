7224e2c04f2b50d80a07c3dca7504d1a216ad3a1
Ivan Tabarino <tabarino@outlook.com>
2021-06-02 16:25:38 +0100

    The keyof Constraint

--- src/app.ts
+++ src/app.ts
@@ -31,3 +31,9 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return `Value: ${obj[key]}`;
+}
+
+console.log(extractAndConvert({ name: 'Ivan' }, 'name'));
