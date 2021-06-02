950d224547b68c24aafffdacd6db1f7d04aaa13e
Ivan Tabarino <tabarino@outlook.com>
2021-06-02 23:51:12 +0100

    Building more useful Decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,22 @@ function Logger(logString: string) {
   }
 }
 
-@Logger('Logging Person')
+function WithTemplate(template: string, hookId: string) {
+  // With "_" we tell typescript that we know there is an argument there, but we won't use it
+  // return function(_: Function) {
+  return function(constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  }
+}
+
+// @Logger('Logging Person')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Ivan';
 
