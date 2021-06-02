ee4008b28c3529ac396f1bad6d4beb3cf7d6a32d
Ivan Tabarino <tabarino@outlook.com>
2021-06-02 15:02:12 +0100

    Working with Constraints

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ promise.then(data => {
   data.split(' ');
 });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
