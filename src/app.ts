f0bbf95a1314b4c9171c05fd00a73c01b6887700
Ivan Tabarino <tabarino@outlook.com>
2021-06-02 10:09:27 +0100

    Creating a Generic Function

--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,10 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then(data => {
   data.split(' ');
 });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Ivan' }, { age: 30 });
+console.log(mergedObj.name);
