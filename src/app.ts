0b58fd9bf122d940ff0e831e2d068a1d21544603
Ivan Tabarino <tabarino@outlook.com>
2021-06-02 15:28:16 +0100

    Another Generic Function

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 const names: Array<string> = [];
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+const promise: Promise<string> = new Promise((resolve) => {
   setTimeout(() => {
     resolve('This is done!');
   }, 1000);
@@ -16,3 +17,17 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: 'Ivan' }, { age: 30 });
 console.log(mergedObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = 'Got no elements.';
+  if (element.length > 0) {
+    description = `Got ${element.length} elements.`;
+  }
+  return [element, description];
+}
+
+console.log(countAndDescribe(['Sports', 'Cooking']));
