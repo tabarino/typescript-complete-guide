f31802018133ef2300d6e0c89025f3d5db81667c
Ivan Tabarino <tabarino@outlook.com>
2021-06-07 22:19:07 +0100

    Add class-transformer to the Project

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import { Product } from './product.model';
 
 declare var GLOBAL: string;
 
 console.log(_.shuffle([1, 2, 3]));
 
 console.log(GLOBAL);
+
+const products = [{ title: 'A Carpet', price: 29.99 }, { title: 'A Book', price: 12.99 }];
+// const p1 = new Product('A Book', 12.99);
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// });
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation()); 
+}
